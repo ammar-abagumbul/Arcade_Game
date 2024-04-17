1d174cb74179d0b844a61f897c9cb7bf57dc5585
aosaf-e-c <aosaf@connect.hku.hk>
2024-04-17 19:24:27 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <ncurses/ncurses.h>
+#include "ncurses/curses.h"
 
 using namespace std;
 
 int main(){
     initscr();
     refresh();
-    //getch();
+    getch();
     endwin();
     return 0;
 }
