5f6ced2c1e633303c77e235a79d3514483c7ab39
aosaf-e-c <aosaf@connect.hku.hk>
2024-04-26 19:43:42 +0800

    Improved formatting of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+// This file contains the main program which initializes
+// the game and starts up the game play depending on the
+// user choice in the main menu
+
 #include <iostream>
+
 #include "../include/animations.h"
 #include "../include/menu_screen.h"
 #include "../include/gameplay.h"
@@ -7,21 +12,35 @@ using namespace std;
 
 int main()
 {
+    // Display the intro animations welcoming the user
     introAnimations();
+
+
+    // The user will always be redirected to the main menu
+    // unless the user quits
     while (true)
     {
+        // The main menu function will return which option
+        // was selected
         int currentOption = showMenuScreen();
-        cout<<currentOption;
-        if (currentOption == 1){
+
+        if (currentOption == 1)
+        {
+            // Start a new game
             startGame();
         }
         else if (currentOption == 3)
         {
+            // Print the game credits
             printCredits();
+
+            // To deal with "Press ENTER to continue"
             cin.ignore();
             cin.ignore();
         }
-        else{
+        else
+        {
+            // Exit from the game
             break;
         }
     }
