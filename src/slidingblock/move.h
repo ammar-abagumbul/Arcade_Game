302497a03a3f0f0bbcd1c33303e93b421a3bf43f
aosaf-e-c <aosaf@connect.hku.hk>
2024-04-30 16:54:48 +0800

    Change include positioning

--- src/slidingblock/move.h
+++ src/slidingblock/move.h
@@ -1,13 +1,18 @@
-#ifndef MOVE_H
-#define MOVE_H
-
 #include <vector>
 #include <string>
 #include <cctype>
+
+#ifndef MOVE_H
+#define MOVE_H
+
 using namespace std;
 
 vector<string> possiblemovecheck(int board[3][3]);
+
+// Not required with current implementation but can
+// be used for future developments
 // int lowerString(int input);
+
 bool checklegal(string input, vector <string> possiblemoves);
 void tileSwap(int (&board)[3][3],int emptyrow,int emptycol,int tilerow,int tilecol);
 void makemove(string input, int (&board)[3][3]);
